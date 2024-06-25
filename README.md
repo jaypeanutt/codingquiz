95976bdac0bce0fd9ad7511bb480fdfde68debd1
angel escobedo <153581828+jaypeanutt@users.noreply.github.com>
2024-06-24 17:01:04 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Coding Trivia Game
+
+## Description
+
+This is a coding quiz made to make coding fun.
+
+## Table of Contents
+
+## Installation
+
+
+
+## Usage
+
+
+
+![alt text](C:\Users\angel\Desktop\coding quiz\image.png)
+
+
---
+++ script.js
@@ -0,0 +1,134 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answers: [
+      { text: '<script>', correct: true },
+      { text: '<scripting>', correct: false }
+    ]
+  },
+  {
+    question: 'How do you create a function in JavaScript?',
+    answers: [
+      { text: 'function myFunction()', correct: true },
+      { text: 'function = myFunction()', correct: false },
+      { text: 'function: myFunction()', correct: false },
+     
+    ]
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'How can you add a comment in a JavaScript?',
+    answers: [
+      { text: '<!--This is a comment-->', correct: false },
+      { text: '//This is a comment', correct: true }
+    ]
+  }
+]
+
+function startTimer() {
+    timerContainer.textContent = "Time: " + timeLeft;
+    timerInterval = setInterval(() => {
+      timeLeft--;
+      timerContainer.textContent = "Time: " + timeLeft;
+      if (timeLeft <= 0) {
+        endQuiz();
+      }
+    }, 1000);
+  }
+  
+  // Function to end the quiz
+  function endQuiz() {
+    clearInterval(timerInterval);
+    quizContainer.style.display = "none";
+    endScreen.style.display = "block";
+    scoreForm.style.display = "block";
+    initialsInput.focus();
+    scoreForm.addEventListener("submit", saveScore);
+  }
+  
